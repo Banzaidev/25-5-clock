bf7b9f6f51132bfd436b2210298657cf2aedeca6
Banzaidev <66788936+Banzaidev@users.noreply.github.com>
2024-01-03 14:13:03 +0100

    The timer works properly, but one of  FCC tests doesn' pass

--- src/App.jsx
+++ src/App.jsx
@@ -5,37 +5,88 @@ export default function App(){
     const [session,setSession] = useState({
         mm: 25,
         ss: 0,
-        value: 25 //the value that appears in the UI
+        value: 25 //used for reset session
     })
     const [pause, setPause] = useState({
         mm: 5,
         ss: 0,
-        value: 5 //the value that appears in the UI
+        value: 5 //used for reset pause
     })
 
     const [statusTimer, setStatusTimer] = useState('play')
+    const [sessionOrPause, setSessionOrPause] = useState('Session') // set Session or Break in timer-label
+    const audioTag = document.querySelector('#beep')
 
-    useEffect(()=> {
+    function controlSessionPauseInterval(state, setStateFunc){
         let interval;
+        if(state.ss != 0){
+            interval = setInterval(()=>{
+                setStateFunc((prevState) => {
+                    return({...prevState, ss: prevState.ss - 1})
+                })
+            }, 1000)
+            
+        }else{
+            if(state.mm > 0){
+                setTimeout(() => {
+                    setStateFunc((prevState) => {return({...prevState, mm: prevState.mm - 1, ss: 59})})
+                }, 1000) 
+            }
+
+        }
+        
+        return interval
+
+    }
+    const awaitAudioPlayed = async (audio) => {
+        return new Promise((resolve) => {
+            setTimeout(() => {
+                resolve('Resolved')
+            }, audio.duration * 1000);
+        })
+    }
+    useEffect(()=> {
+
+        let intervalSession;
+        let intervalPause;
         if(statusTimer == 'pause'){
-            console.log(session)
-            if(session.ss != 0){
-                interval = setInterval(()=>{
-                    setSession((prevSession) => {
-                        return({...prevSession, ss: prevSession.ss - 1})
-                    })
-                }, 1000)
+            
+            (async () => {
                 
-            }else{
-                if(session.mm > 0){
-                    console.log('0s')
-                    setSession((prevSession) => {return({...prevSession, mm: prevSession.mm - 1, ss: 59})})
+                if((session.mm == 0 && session.ss == 0) && (pause.mm == 0 && pause.ss == 0)){
+                    handleReset(false, session.value, pause.value, true)
                 }
 
-            }
-            console.log(session)
+                if(session.mm == 0 && session.ss == 0){
+                    if(sessionOrPause != 'Break'){
+                        audioTag.play()
+                        setSessionOrPause('Break')
+                        await awaitAudioPlayed(audioTag)
+                        
+                    }else{
+                        intervalPause = controlSessionPauseInterval(pause,setPause)
+                    }
+                    
+                    if(pause.mm == 0 && pause.ss == 0){
+                        audioTag.play()
+                        await awaitAudioPlayed(audioTag)
+                        setSessionOrPause('Session')
+                    }
+
+                }else{
+                    if(sessionOrPause == 'Session'){
+                        intervalSession = controlSessionPauseInterval(session, setSession)   
+                    }
+                }
+
+            })()
+
+
         }
-        return () => clearInterval(interval)
+        return () => {
+            clearInterval(intervalSession)
+            clearInterval(intervalPause)
+        }
 
 
     })
@@ -61,6 +112,8 @@ export default function App(){
         })
     }
 
+    const setValue = (setStateFunc) => setStateFunc((prevState) => {return({...prevState, value: prevState.mm})})
+
     const handleSessionBreakControls = (e) => {
         const sessionOrBreak = (e.target.id.split('-'))[0] //session or break
         const incrementOrDecrement = (e.target.id.split('-'))[1] // increment or decrement
@@ -71,18 +124,15 @@ export default function App(){
                 }else{
                     decrementState(setSession)
                 }
-                setSession((prevSession) => {return({...prevSession, value: prevSession.mm})})
-                console.log(session)
+                setValue(setSession)
                 break
             case 'break':
                 if(incrementOrDecrement == 'increment'){
                     incrementState(setPause)
-                    console.log(pause)
                 }else{
                     decrementState(setPause)
                 }
-                setPause((prevPause) => {return({...prevPause, value: prevPause.mm})})
-                console.log(pause)
+                setValue(setPause)
                 break
 
         }
@@ -97,19 +147,52 @@ export default function App(){
         }
     }
 
-    const handleReset = () => {
+    const handleReset = (pauseTimer = true, mmSession = 25, mmPause = 5, buttonClicked = true) => {
+        if(statusTimer == 'pause' && pauseTimer){
+            setStatusTimer('play')
+        }
         setSession({
-            mm: 25,
+            mm: mmSession,
             ss: 0,
-            value: 25
+            value: mmSession
         })
         setPause({
-            mm: 5,
+            mm: mmPause,
             ss: 0,
-            value: 5
+            value: mmPause
         })
+        audioTag.pause()
+        audioTag.currentTime = 0;
+        if(buttonClicked){
+            setSessionOrPause('Session')
+        }
+        
+        
     }
 
+    const timerToBeShowed = (state) => {
+        let minutes;
+        let seconds;
+
+        if(state.mm == 0) {
+            minutes = '00'
+        }else if(state.mm.toString().length == 1) {
+            minutes = `0${state.mm}`
+        }else{
+            minutes = state.mm
+        }
+
+        if(state.ss == 0){
+            seconds = '00'
+        }else if(state.ss.toString().length == 1){
+            seconds = `0${state.ss}`
+        }else{
+            seconds = state.ss
+        }
+
+        return(`${minutes}:${seconds}`)
+    
+    }
 
     return(
         <>
@@ -127,14 +210,16 @@ export default function App(){
                 <button onClick={(e) => handleSessionBreakControls(e)} id="session-increment">+</button>
             </div>
             <div id='timer'>
-                <p id='timer-label'>Session</p>
-                <p id='timer-left'>{(session.mm == 0 && session.ss == 0) ? pause : `${session.mm}:${session.ss}`}</p>
+                <p id='timer-label'>{sessionOrPause}</p>
+                <p id='time-left'>
+                    {sessionOrPause == 'Session' ? timerToBeShowed(session) : timerToBeShowed(pause)}
+                </p>
             </div>
             <div id='timer-controls'>
                 <button onClick={(e) => handlePlayStop(e)} id='start_stop'>{statusTimer}</button>
                 <button onClick={() => handleReset()}id='reset'>reset</button>
             </div>
-            <audio id='beep'></audio> 
+            <audio loop={false} preload="auto" src='https://cdn.freecodecamp.org/testable-projects-fcc/audio/BeepSound.wav' id='beep'></audio> 
         </>
     )
 }
